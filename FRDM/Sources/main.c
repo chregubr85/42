0071755429cd04e4c1df26ee596e5893da8d9a33
S.Portmann <stefan.portmann@stud.hslu.ch>
2014-09-19 16:22:48 +0200

    improved main.c with fancy program :-)

--- FRDM/Sources/main.c
+++ FRDM/Sources/main.c
@@ -49,7 +49,9 @@ int main(void)
 
   /* Write your code here */
   /* For example: for(;;) { } */
-
+  uint16_t a,b;
+  a=5;
+  b=2*a;
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
   #ifdef PEX_RTOS_START
