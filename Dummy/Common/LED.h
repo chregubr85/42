73ca8994010336a47d8caeacf8f39a2228d57bfb
S.Portmann <stefan.portmann@stud.hslu.ch>
2014-09-26 14:21:08 +0200

    improved led.h

--- Dummy/Common/LED.h
+++ Dummy/Common/LED.h
@@ -10,38 +10,60 @@
 
 
 #include "platform.h"
+#include "LEDBit1.h"
+#include "LEDBit2.h"
+#include "LEDBit3.h"
 
 #if PL_NOF_LEDS >= 1
-  #include "LEDblue.h"
-  #define LEDBLUE_On()     LedBit1_ClrVal()
+
+  #define LED1_On()     LedBit1_ClrVal()
     /*!< Turn LED 1 on */
-  #define LEDBLUE_Off()    LedBit1_SetVal()
+  #define LED1_Off()    LedBit1_SetVal()
     /*!< Turn LED 1 off */
-  #define LEDBLUE_Neg()    LedBit1_NegVal()
+  #define LED1_Neg()    LedBit1_NegVal()
     /*!< Toggle LED 1 */
-  #define LEDBLUE_Get()    LedBit1_GetVal()
+  #define LED1_Get()    LedBit1_GetVal()
     /*!< Return TRUE if LED is on, FALSE otherwise */
-  #define LEDBLUE_Put(val) LedBit1_PutVal(val)
+  #define LED1_Put(val) LedBit1_PutVal(val)
     /*!< Turn LED on or off */
-  #define LEDBLUE_Init()   /* do nothing */
+  #define LED1_Init()   /* do nothing */
     /*!< Initialize LED */
-  #define LEDBLUE_Deinit() /* do nothing */
+  #define LED1_Deinit() /* do nothing */
     /*!< De-Initialize LED */
-#else
-  #define LEDBLUE_On()     /* do nothing */
+#endif
+
+#if PL_NOF_LEDS >= 2
+  #define LED2_On()     LedBit2_ClrVal()
     /*!< Turn LED 1 on */
-  #define LEDBLUE_Off()    /* do nothing */
+  #define LED2_Off()    LedBit2_SetVal()
     /*!< Turn LED 1 off */
-  #define LEDBLUE_Neg()    /* do nothing */
+  #define LED2_Neg()    LedBit2_NegVal()
     /*!< Toggle LED 1 */
-  #define LEDBLUE_Get()  0 /* do nothing */
+  #define LED2_Get()    LedBit2_GetVal()
     /*!< Return TRUE if LED is on, FALSE otherwise */
-  #define LEDBLUE_Put(val) /* do nothing */
-    /*!< Turn LED 1 on or off */
-  #define LEDBLUE_Init()   /* do nothing */
-    /*!< Initialize LED 1 */
-  #define LEDBLUE_Deinit() /* do nothing */
-    /*!< De-Initialize LED 1 */
+  #define LED2_Put(val) LedBit2_PutVal(val)
+    /*!< Turn LED on or off */
+  #define LED2_Init()   /* do nothing */
+    /*!< Initialize LED */
+  #define LED2_Deinit() /* do nothing */
+    /*!< De-Initialize LED */
+#endif
+
+#if PL_NOF_LEDS == 3
+  #define LED3_On()     LedBit3_ClrVal()
+    /*!< Turn LED 1 on */
+  #define LED3_Off()    LedBit3_SetVal()
+    /*!< Turn LED 1 off */
+  #define LED3_Neg()    LedBit3_NegVal()
+    /*!< Toggle LED 1 */
+  #define LED3_Get()    LedBit3_GetVal()
+    /*!< Return TRUE if LED is on, FALSE otherwise */
+  #define LED3_Put(val) LedBit3_PutVal(val)
+    /*!< Turn LED on or off */
+  #define LED3_Init()   /* do nothing */
+    /*!< Initialize LED */
+  #define LED3_Deinit() /* do nothing */
+    /*!< De-Initialize LED */
 #endif
 
 #endif /* LED_H_ */
