e4504772e7254800e6e1e37efb92c6e31e87ccc4
Christian Brunner <christian.brunner@stud.hslu.ch>
2014-11-21 13:52:17 +0100

    Bugfix Tacho

--- Dummy/Common/Tacho.c
+++ Dummy/Common/Tacho.c
@@ -18,12 +18,12 @@
 #include "FRTOS1.h"
 
   /*! \todo Set appropriate values for NOF_ROUND_PULSE, TACHO_SAMPLE_PERIOD_MS and NOF_HISTORY */
-#define NOF_ROUND_PULSE      (3*4)
+#define NOF_ROUND_PULSE      (3*4*75)
   /*!< \todo number of pulses per round, given by encoder hardware, and we are detecting 4 steps for each period. Useful to calculate revolutions per time unit. */
 #define TACHO_SAMPLE_PERIOD_MS (1)
   /*!< \todo speed sample period in ms. Make sure that speed is sampled at the given rate. */
 //#define NOF_HISTORY (16U+1U)
-#define NOF_HISTORY (4U)
+#define NOF_HISTORY (16+1U)
   /*!< number of samples for speed calculation (>0):the more, the better, but the slower. */
 
 static volatile uint16_t TACHO_LeftPosHistory[NOF_HISTORY], TACHO_RightPosHistory[NOF_HISTORY];
